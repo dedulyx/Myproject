7707d3dfb898cc0fe169eb8c6653ceb940423474
Igor <igo9505@mail.ru>
2022-10-13 19:00:39 +0300

    Modified

--- Test2.java
+++ Test2.java
@@ -2,6 +2,6 @@ public class Test2 implements Info {
 
     @Override
     public void go() {
-        System.out.println("Hello Test222");
+        System.out.println("Hello Test JavaWork");
     }
 }
